c41037067bff2d536047464d0e6ee629bc607bd9
Renan Santos <rs.ls2408@gmail.com>
2023-12-04 10:37:25 -0300

    tailwind added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,6 @@
-"use client";
 import Link from "next/link";
 import React from "react";
-import styled from "styled-components";
 
-const NavBar = styled.nav`
-  width: 100%;
-  height: 40px;
-  padding: 10px 60px;
-  border-bottom: 1px solid black;
-`;
-const NavUl = styled.ul`
-  display: flex;
-  gap: 20px;
-  list-style-type: none;
-`;
-
-const LiLink = styled(Link)`
-  min-width: 100px;
-`;
 type Rota = {
   name: string;
   link: string;
@@ -43,14 +26,16 @@ export default function Navbar() {
     },
   ];
   return (
-    <NavBar>
-      <NavUl>
+    <nav className="w-screen h-12 px-3 py-3 border-b border-gray-500">
+      <ul className="flex gap-20 list-none">
         {routes.map((rota: Rota) => (
-          <li>
-            <LiLink href={rota.link}>{rota.name}</LiLink>
+          <li key={rota.link}>
+            <Link className="min-w-100" href={rota.link}>
+              {rota.name}
+            </Link>
           </li>
         ))}
-      </NavUl>
-    </NavBar>
+      </ul>
+    </nav>
   );
 }
